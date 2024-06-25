06be79e7b16a150b044e8088fe63f7cb2d780e40
SouryaSparks <vzoid8@gmail.com>
2024-06-24 17:31:14 -0700

    Fixed Changes Classify.py

--- classify.py
+++ classify.py
@@ -6,23 +6,25 @@ from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 import cv2
 
-def read_img(image):
-    img_array = cv2.imread(image)
 
-    return img_array
+MODEL = load_model(os.path.join('models','Mask1'))
+
+
+from keras.preprocessing import image
+
+img = 'image.png'
 
 
 classes = ['Wearing Mask','Not Wearing Mask']
 
-img_dir = 'image.filetype'
 
-img = read_img(img_dir)
-img = tf.image.resize(img,(256,256))
+test_image = image.load_img(img)
 
-# plt.imshow(img)
-MODEL = load_model(os.path.join('models','Mask1'))
+test_image = image.img_to_array(test_image)
 
+test_image = tf.image.resize(test_image,(256,256))
 
-yhat = MODEL.predict(np.expand_dims((img),0))
-print(classes[np.argmax(yhat[0])])
-print(f'Confidence: {np.max(yhat[0])}')
+predict = MODEL.predict(np.expand_dims(test_image,0))
+
+print(classes[np.argmax(predict)])
+print(f"Confidence: {np.max(predict)}")
